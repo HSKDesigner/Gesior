b731ca2fe4d28af9eddf217ddde0d8e34e6b63e1
HSKDesigner <rickserraglia@gmail.com>
2015-06-14 12:33:50 -0300

    Update team.php

--- team.php
+++ team.php
@@ -5,30 +5,30 @@ if(!defined('INITIALIZED'))
 $list = $SQL->query('SELECT ' . $SQL->fieldName('name') . ', ' . $SQL->fieldName('id') . ', ' . $SQL->fieldName('group_id') . ' FROM ' . $SQL->tableName('players') . ' WHERE ' . $SQL->fieldName('group_id') . ' IN (' . implode(',', $config['site']['groups_support']) . ') ORDER BY ' . $SQL->fieldName('group_id') . ' DESC');
 
 
-$main_content .= "
+$main_content .= '
 The website www.tibia.com and the associated online game Tibia are operated by CipSoft GmbH, Germany.
 </br></br>
-<table width=\"100%\" border=\"0\" cellspacing=\"1\" cellpadding=\"4\">
-<tbody><tr><td bgcolor=\"#505050\" align=\"center\" class=\"white\"><b>Contact Information</b></td></tr>
-<tr><td bgcolor=\"#D4C0A1\"><table border=\"0\" cellpadding=\"8\">
-<tbody><tr><td valign=\"top\">Address:</td><td>CipSoft GmbH<br>Pr&#252;feninger Stra&#223;e 20<br>93049 Regensburg<br>Germany</td></tr>
-<tr><td>Support Email:</td><td><a href=\"mailto:\">support@tibia.com</a></td></tr>
-<tr><td>Company Website:</td><td><a href=\"http://www.cipsoft.com\">www.cipsoft.com</a></td></tr>
+<table width="100%" border="0" cellspacing="1" cellpadding="4">
+<tbody><tr><td bgcolor="'.$config['site']['vdarkborder'].'" align="center" class="white"><b>Contact Information</b></td></tr>
+<tr><td bgcolor="'.$config['site']['darkborder'].'"><table border="0" cellpadding="8">
+<tbody><tr><td valign="top">Address:</td><td>CipSoft GmbH<br>Pr&#252;feninger Stra&#223;e 20<br>93049 Regensburg<br>Germany</td></tr>
+<tr><td>Support Email:</td><td><a href="mailto:">support@tibia.com</a></td></tr>
+<tr><td>Company Website:</td><td><a href="http://www.cipsoft.com">www.cipsoft.com</a></td></tr>
 <tr><td>Managing Directors:</td><td>Ulrich Schlott and Stephan Vogler</td></tr>
-<tr><td valign=\"top\">Legal Information:</td><td>based in Regensburg<br>registered at Amtsgericht Regensburg, HRB 8295<br>Ust-IdNr.: DE 216262082</td></tr>
+<tr><td valign="top">Legal Information:</td><td>based in Regensburg<br>registered at Amtsgericht Regensburg, HRB 8295<br>Ust-IdNr.: DE 216262082</td></tr>
 </tbody></table></td></tr>
-</tbody></table></br></br>";
-$main_content .= "<table width=\"100%\" border=\"0\" cellspacing=\"1\" cellpadding=\"4\">
-<tbody><tr><td bgcolor=\"#505050\" align=\"center\" class=\"white\"><b>Copyright</b></td></tr>
-<tr><td bgcolor=\"#D4C0A1\"><table border=\"0\" cellpadding=\"8\"><tbody><tr><td>
+</tbody></table></br></br>';
+$main_content .= '<table width="100%" border="0" cellspacing="1" cellpadding="4">
+<tbody><tr><td bgcolor="'.$config['site']['vdarkborder'].'" align="center" class="white"><b>Copyright</b></td></tr>
+<tr><td bgcolor="'.$config['site']['darkborder'].'"><table border="0" cellpadding="8"><tbody><tr><td>
 <b>Tibia</b><sup>&#174;</sup> - a massively multiplayer online role-playing game.<br>
 Copyright &#169; 1996-2015 CipSoft GmbH. All rights reserved.<br>
 Tibia is a registered trademark of CipSoft GmbH.
 </td></tr></tbody></table></td></tr>
-</tbody></table></br></br>";
-$main_content .= "<table width=\"100%\" border=\"0\" cellspacing=\"1\" cellpadding=\"4\">
-<tbody><tr><td bgcolor=\"#505050\" align=\"center\" class=\"white\"><b>Disclaimer</b></td></tr>
-<tr><td bgcolor=\"#D4C0A1\"><table border=\"0\" cellpadding=\"8\"><tbody><tr><td>
+</tbody></table></br></br>';
+$main_content .= '<table width="100%" border="0" cellspacing="1" cellpadding="4">
+<tbody><tr><td bgcolor="'.$config['site']['vdarkborder'].'" align="center" class="white"><b>Disclaimer</b></td></tr>
+<tr><td bgcolor="'.$config['site']['darkborder'].'"><table border="0" cellpadding="8"><tbody><tr><td>
 CipSoft GmbH disclaims all warranties for the up-to-dateness, correctness,
 completeness or quality of the information presented on this website.
 CipSoft GmbH is not liable for any lost profits or special, incidental
@@ -46,14 +46,14 @@ take responsibility for the up-to-dateness, correctness, completeness or
 general quality of the information supplied by these websites. Also,
 Cipsoft GmbH expressly disassociates itself from any content presented on
 said websites. This declaration applies to any link to external websites to
-be found on one or more of CipSoft's websites, as well as to any kind of
+be found on one or more of CipSoft\'s websites, as well as to any kind of
 content these external websites may contain.
 </td></tr></tbody></table></td></tr>
-</tbody></table></br></br>";
+</tbody></table></br></br>';
 $bgcolor = (($i++ % 2 == 1) ?  $config['site']['darkborder'] : $config['site']['lightborder']);
-$main_content .= "<table width=\"100%\" border=\"0\" cellspacing=\"1\" cellpadding=\"4\">
-      <tbody><tr><td bgcolor=\"#505050\" class=\"white\" colspan=\"3\"><b>CipSoft Members</b></td></tr>
-      <tr bgcolor=\"#D4C0A1\"><td width=\"100%\"><b>Name</b></td><td><b>Group</b></td><td>&nbsp;</td></tr>";
+$main_content .= '<table width="100%" border="0" cellspacing="1" cellpadding="4">
+      <tbody><tr><td bgcolor="'.$config['site']['vdarkborder'].'" class="white" colspan="3"><b>CipSoft Members</b></td></tr>
+      <tr bgcolor="'.$config['site']['darkborder'].'"><td width="100%"><b>Name</b></td><td><b>Group</b></td><td>&nbsp;</td></tr>';
 
 foreach($list as $i => $supporter)
 {
@@ -65,4 +65,4 @@ foreach($list as $i => $supporter)
 	$main_content .= '<tr bgcolor="'.$bgcolor.'"><td>'.htmlspecialchars($supporter['name']).'</td><td><nobr>' . htmlspecialchars(Website::getGroupName($supporter['group_id'])) . '<nobr></td><td><a href="?subtopic=characters&name='.urlencode($supporter['name']).'"><img src="'.$layout_name.'/images/buttons/sbutton_view.gif" alt="'.htmlspecialchars($supporter['name']).'"></a></td></tr>';
 }
 
-$main_content .= "</td></tr>  </tbody></table>";
+$main_content .= '</td></tr>  </tbody></table>';
